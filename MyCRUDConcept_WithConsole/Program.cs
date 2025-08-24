d4a45ba4abf132df8d361e44fba9d711a1c5df7b
Gino <ggino3567@gmail.com>
2025-08-24 11:23:36 +0800

    Update: 更新了do-while 還很簡陋 回家再弄 btw今天超厭世ㄉ [2025/8/24 11:23]

--- MyCRUDConcept_WithConsole/Program.cs
+++ MyCRUDConcept_WithConsole/Program.cs
@@ -7,20 +7,72 @@ class Program
     static void Main(string[] args)
     {
         //2025/8/24 : 考慮後面加上do-while 然後再用選項去跟隨if-else或switch來不斷執行CRUD
+        //2025/8/24 : 加了 有點簡陋 回來再改完整點
 
         CallCRUD sample = new CallCRUD();
-        sample.AddToList(1, "One");
-        sample.AddToList(2, "Two");
-        sample.AddToList(3, "Three");
-        sample.AddToList(10, "Ten");
-        sample.AddToList(25, "Twenty Five");
-        sample.RemoveFromList(3);
+        
+        do
+        {
+            Console.WriteLine("==== SimpleCRUD ====");
+            Console.WriteLine("1. 添加");
+            Console.WriteLine("2. 讀取");
+            Console.WriteLine("3. 更新");
+            Console.WriteLine("4. 刪除");
+            Console.WriteLine("5. 退出");
+            Console.WriteLine("====================");
+            Console.WriteLine("\n請選擇執行動作 : ");
+            int operatorInput = int.Parse(Console.ReadLine());
 
-        sample.ShowList();
-        sample.UpdateElement(25, "This is my fucking age.");
-        sample.AddToList(2, "TWOOOOO");
-        sample.ShowList();
-        sample.GetElemet(25);
+            if (operatorInput == 5)
+            {
+                Console.WriteLine("\n===> 退出");
+                return;
+            }
+            else if (operatorInput == 1)
+            {
+                Console.WriteLine("\n===> 添加");
+                Console.Write("輸入ID : ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("\n輸入Value : ");
+                string name = Console.ReadLine();
+                sample.AddToList(id, name);
+            }
+            else if (operatorInput == 2)
+            {
+                Console.WriteLine("\n===> 讀取");
+                Console.Write("請輸入 (1) to 讀取全部 | (2) 讀取特定內容 : ");
+                int chooseInput = int.Parse(Console.ReadLine());
+                if (chooseInput == 1)
+                {
+                    sample.ShowList();
+                }
+                else
+                {
+                    Console.Write("輸入欲搜尋的ID : ");
+                    int id = int.Parse(Console.ReadLine());
+                    sample.GetElemet(id);
+                }
+            }
+            else if (operatorInput == 3)
+            {
+                Console.Write("輸入ID : ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("\n輸入Value : ");
+                string name = Console.ReadLine();
+                sample.UpdateElement(id, name);
+            }
+            else if (operatorInput == 4)
+            {
+                Console.Write("輸入欲搜尋的ID : ");
+                int id = int.Parse(Console.ReadLine());
+                sample.RemoveFromList(id);
+            }
+            else
+            {
+                Console.WriteLine("輸入的選項錯誤!");
+            }
+
+        } while (true);
     }
 }
 
@@ -39,6 +91,7 @@ class CallCRUD
             return;
         }
         Members.Add(id, name);
+        Console.WriteLine($"Members[{id}, {name}] add success!\n");
     }
 
     //D
@@ -70,7 +123,7 @@ class CallCRUD
     {
         if (Members.Count == 0)
         {
-            Console.WriteLine("\n===> No members in the list.");
+            Console.WriteLine("\n===> No members in the list.\n");
             return;
         }
 
